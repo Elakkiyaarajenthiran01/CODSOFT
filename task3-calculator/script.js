10e1d73634b53d123d4172f72c55b15288e271f4
Elakkiyaa <elakkiyaarajenthiran@gmail.com>
2026-07-12 16:39:17 +0530

    Add Task 3: Calculator using HTML CSS and JavaScript

---
+++ task3-calculator/script.js
@@ -0,0 +1,26 @@
+let display = document.getElementById('display');
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    let result = eval(display.value);
+    if (result === undefined || isNaN(result)) {
+      display.value = 'Error';
+    } else {
+      display.value = result;
+    }
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
